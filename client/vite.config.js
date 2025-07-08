96ff29157b0f1076379c0b748da19e6862953b33
Victor Barreto <gilabarreto@gmail.com>
2025-07-08 17:09:36 -0600

    Fix vite config file.

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/concertfyi2000/' : '/',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
