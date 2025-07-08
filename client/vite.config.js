fa3da53d976fe169a267389e86eb5c02f47710b0
Victor Barreto <gilabarreto@gmail.com>
2025-07-08 14:42:43 -0600

    Deploy.

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? '/concertfyi2000/' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
