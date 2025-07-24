72ab894c2504989fe271f685e1aa85c89b35d58d
Victor Barreto <gilabarreto@gmail.com>
2025-07-24 16:18:26 -0600

    Dry and Clean Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,16 +1,17 @@
 export default function Header() {
   return (
-    <div className="flex flex-1 flex-col h-[120px] max-h-min justify-center text-3xl font-medium tracking-tight items-center pt-6">
-      <h1>
+    <div
+      className="flex flex-1 flex-col h-[120px] max-h-min justify-center items-center pt-6 text-center"
+    >
+      <h1 className="text-3xl font-medium tracking-tight">
         concert{"{"}
-        <span className="text-3xl tracking-tight font-semibold text-zinc-100">
-          fyi
-        </span>
+        <span className="text-3xl font-semibold text-zinc-100">fyi</span>
         {"}"}
       </h1>
-      <h1 className="text-4xl top-0 font-bold text-center text-zinc-100 tracking-tight pt-4">
+
+      <h2 className="text-4xl font-bold text-zinc-100 tracking-tight pt-4">
         Live Music Lives Here.
-      </h1>
+      </h2>
     </div>
   );
 }
