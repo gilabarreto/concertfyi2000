74d573b4f36ce9bae1d1e4c8dce2cf7082433eaf
Victor Barreto <gilabarreto@gmail.com>
2025-07-26 17:29:30 -0600

    Dry and Clean Setlist.jsx

--- client/src/components/ArtistPage/Setlist.jsx
+++ client/src/components/ArtistPage/Setlist.jsx
@@ -3,27 +3,26 @@ import { faMusic, faCirclePlay } from "@fortawesome/free-solid-svg-icons";
 import { faFileLines } from "@fortawesome/free-regular-svg-icons";
 import { faCircleInfo } from "@fortawesome/free-solid-svg-icons";
 
-
 export default function Setlist(props) {
   const songs = props.concert.sets.set[0]?.song || [];
   const artistName = props.concert.artist.name;
 
   return (
     <>
-      <div className="flex flex-1 justify-around mb-2">
-        <h2 className="flex-1 text-4xl font-bold mb-2">Setlist</h2>
-        <span className="flex">
-          <FontAwesomeIcon
-            icon={faCircleInfo}
-            className="cursor-pointer text-gray-500"
-            size="lg"
-          />
-        </span>
+      <div className="flex flex-1 justify-between items-center mb-2">
+        <h2 className="text-4xl font-bold">Setlist</h2>
+        <FontAwesomeIcon
+          icon={faCircleInfo}
+          className="text-gray-500 cursor-pointer"
+          size="lg"
+        />
       </div>
-      <hr className="border-t border-gray-300 opacity-50 ml-6"></hr>
+
+      <hr className="border-t border-gray-300 opacity-50 ml-6" />
+
       <ol className="list-decimal list-inside pl-6">
         {songs.length === 0 ? (
-          <span className="py-2">
+          <span className="py-2 block text-gray-500">
             There are no songs in this setlist. Please come back later.
           </span>
         ) : (
@@ -46,23 +45,27 @@ export default function Setlist(props) {
                   </span>
                   <span>{songName}</span>
                 </span>
+
                 <span className="flex items-center space-x-3">
                   <a href={spotifyUrl} target="_blank" rel="noreferrer">
                     <FontAwesomeIcon
                       icon={faMusic}
                       className="text-red-600 hover:text-red-800"
+                      title="Spotify"
                     />
                   </a>
                   <a href={youtubeUrl} target="_blank" rel="noreferrer">
                     <FontAwesomeIcon
                       icon={faCirclePlay}
                       className="text-red-600 hover:text-red-800"
+                      title="YouTube"
                     />
                   </a>
                   <a href={lyricsUrl} target="_blank" rel="noreferrer">
                     <FontAwesomeIcon
                       icon={faFileLines}
                       className="text-red-600 hover:text-red-800"
+                      title="Lyrics"
                     />
                   </a>
                 </span>
