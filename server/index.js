5b6241071391b651c797728973970164bbd69122
Victor Barreto <gilabarreto@gmail.com>
2025-07-28 17:56:50 -0600

    Dry and Clean index.js server-side.

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+
 const allowedOrigins = [
   "https://gilabarreto.github.io",
   "https://concertfyi.com",
@@ -11,11 +12,13 @@ const allowedOrigins = [
 ];
 
 app.use(express.json());
-app.use(cors({
-  origin: allowedOrigins,
-  credentials: true,
-}));
 
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
 
 app.use("/api/ticketmaster", require("./routes/ticketmaster"));
 app.use("/api/setlist", require("./routes/setlist"));
@@ -23,6 +26,3 @@ app.use("/api/setlist", require("./routes/setlist"));
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
-
-console.log("SETLISTFM_API_KEY:", process.env.SETLISTFM_API_KEY);
-console.log("TICKETMASTER_API_KEY:", process.env.TICKETMASTER_API_KEY);
