d3eb2e8a369828e9969d2f043ff3f448045c2bf8
gilabarreto <gilabarreto@users.noreply.github.com>
2026-09-23 06:31:08 +0000

    deploy: faced60cfa4ff79d571ded4e6413a3051f2e8bd5

--- assets/ArtistPage-DdVyTRzq.js
+++ assets/ArtistPage-DdVyTRzq.js
@@ -1,4 +1,4 @@
-import{u as Je,r as i,a as Rs,j as c,I as ee,b as Bs,c as As,f as Us,d as he,e as zs,h as _s,i as $s,k as ot,l as at,m as ts,g as lt,n as ut,o as _e,q as ct,p as ns,s as ss,t as is,v as rs,w as os,x as Vs,y as Fs,z as Zs,B as Nt,C as Ws,D as as,E as Hs,F as Gs,G as Ks,H as qs,P as Js,J as Ys,K as ls,T as Qs,M as Xs,N as ei,L as us,O as ti,A as ni,Q as si,R as ii,U as ri,S as oi}from"./index-DXKxawxC.js";import{f as ai,a as li,b as ui,c as cs}from"./index-CB2dMpb3.js";/*!
+import{u as Je,r as i,a as Rs,j as c,I as ee,b as Bs,c as As,f as Us,d as he,e as zs,h as _s,i as $s,k as ot,l as at,m as ts,g as lt,n as ut,o as _e,q as ct,p as ns,s as ss,t as is,v as rs,w as os,x as Vs,y as Fs,z as Zs,B as Nt,C as Ws,D as as,E as Hs,F as Gs,G as Ks,H as qs,P as Js,J as Ys,K as ls,T as Qs,M as Xs,N as ei,L as us,O as ti,A as ni,Q as si,R as ii,U as ri,S as oi}from"./index-B4vdcbX9.js";import{f as ai,a as li,b as ui,c as cs}from"./index-CB2dMpb3.js";/*!
  * Font Awesome Free 7.3.1 by @fontawesome - https://fontawesome.com
  * License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
  * Copyright 2026 Fonticons, Inc.
